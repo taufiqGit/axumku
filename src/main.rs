bd07a47482ff8153e61b170dba72cdb9f70cb1f1
taufiqGit <wecannn29@gmail.com>
2025-03-03 13:09:47 +0700

    eden

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use axum::{
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }))
+                            .route("/pass", get(|| async { "kabir"}));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3002").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
